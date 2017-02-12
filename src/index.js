fdfddaf9c14a7e0526742560df6622df8bab54d3
BeaverGuo <lalabeaver21@gmail.com>
2017-02-12 07:54:36 -0800

    add react-router and auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import SC from 'soundcloud';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
 import Stream from './components/Stream';
+import { CLIENT_ID, REDIRECT_URI } from './constants/auth';
 
+SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
 const tracks = [
     {
         title: 'Some track'
